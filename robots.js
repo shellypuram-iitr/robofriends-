616b1d426901f2b4472b5492ada16d0cf83ba567
shellypuram-iitr <75660759+shellypuram-iitr@users.noreply.github.com>
2021-05-11 02:33:17 +0530

    Add files via upload

---
+++ robots.js
@@ -0,0 +1,63 @@
+export const robots = [
+    {
+      id: 1,
+      name: 'Shelly',
+      username: 'Bret',
+      email: 'Shelly@gmail.com'
+    },
+    {
+      id: 2,
+      name: 'Naman',
+      username: 'Antonette',
+      email: 'Naman@gmail.com'
+    },
+    {
+      id: 3,
+      name: 'Ritik',
+      username: 'Samantha',
+      email: 'Ritik@gmail.com'
+    },
+    {
+      id: 4,
+      name: 'Arpit',
+      username: 'Karianne',
+      email: 'Arpit@gmail.com'
+    },
+    {
+      id: 5,
+      name: 'Ananya',
+      username: 'Kamren',
+      email: 'Ananya@gmail.com'
+    },
+    {
+      id: 6,
+      name: 'Prakhar',
+      username: 'Leopoldo_Corkery',
+      email: 'Prakhar@gmail.com'
+    },
+    {
+      id: 7,
+      name: 'Shrivardhan',
+      username: 'Elwyn.Skiles',
+      email: 'Shri@gmail.com'
+    },
+    {
+      id: 8,
+      name: 'Honey',
+      username: 'Maxime_Nienow',
+      email: 'Honey@gmail.com'
+    },
+    {
+      id: 9,
+      name: 'Ashish',
+      username: 'Delphine',
+      email: 'Ashish@gmail.com'
+    },
+    {
+      id: 10,
+      name: 'Mehul',
+      username: 'Moriah.Stanton',
+      email: 'Mehul@gmail.com'
+    }
+  ];
+  
